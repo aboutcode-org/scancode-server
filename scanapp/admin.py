92b8ec7984dfb23de67c5e224b61dcdaa659b404
RajuKoushik <g.rajukoushik@gmail.com>
2017-06-13 21:39:25 +0530

    Updated admin.py.
    *Registered models

--- scanapp/admin.py
+++ scanapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import user_info, AnonymousUser, code_info, scanned_results, copyright, license, package
+
 # Register your models here.
+admin.site.register(user_info)
+admin.site.register(AnonymousUser)
+admin.site.register(code_info)
+admin.site.register(scanned_results)
+admin.site.register(copyright)
+admin.site.register(license)
+admin.site.register(package)
