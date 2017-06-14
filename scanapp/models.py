f83cea0559e2d1c998e3205856143b24855098ed
singh1114 <ranvir.singh1114@gmail.com>
2017-06-14 12:02:07 +0530

    Change the models to accomodate local scan and scan errors
    * Normalize the database
    * Add a table of scanInfo to use both URL and localscan
    * Add a table of ScanError to accomodate the errors.
    * Add some more tables for keeping the database in normalized form
    
    Signed-off-by: Ranvir Singh ranvir.singh1114@gmail.com

--- scanapp/models.py
+++ scanapp/models.py
@@ -30,48 +30,98 @@ class AnonymousUser(models.Model):
         return 'guest-%s' % self.user.id
 
 
+class ScanInfo(models.Model):
+    def __str__(self):
+        return self.scan_type
+    
+    # types of scans that can be applied
+    scan_types = (
+        ('URL', 'URL'),
+        ('Local Scan', 'localscan'),
+    )
+    
+    
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    scan_type = models.CharField(max_length=20, choices=scan_types, default='URL')
+
+
+class URLScanInfo(models.Model):
+    def __str__(self):
+        return self.URL
+
+    scan_info = models.ForeignKey(ScanInfo)
+    URL = models.URLField(max_length=2000)
+
+
+class LocalScanInfo(models.Model):
+    def __str__(self):
+        return self.folder_name
+
+    scan_info = models.ForeignKey(ScanInfo)
+    folder_name = models.CharField(max_length=200)
+
+
 class CodeInfo(models.Model):
     def __str__(self):
-        return self.url
+        return self.total_code_files
 
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    code_url_path = models.CharField(max_length=3000)
-    code_file_number = models.IntegerField(null=True, blank=True)
-    code_file_size = models.IntegerField(null=True, blank=True, default=0)
+    scan_info = models.ForeignKey(ScanInfo)
+    total_code_files = models.IntegerField(null=True, blank=True)
+    code_size = models.IntegerField(null=True, blank=True, default=0)
 
 
-class ScannedResults(models.Model):
+# ScanResult will store the most basic results of the scan
+# Single result for each scan
+class ScanResult(models.Model):
     def __str__(self):
         return self.total_errors
 
     code_info = models.ForeignKey(CodeInfo)
     scanned_json_result = JSONField()
     scanned_html_result = models.CharField(max_length=10000)
-    scanned_files = models.IntegerField(null=True, blank=True, default=0)
+    scancode_notice = models.CharField(max_length=2000)
+    scancode_version = models.CharField(max_length=200)
+    files_count = models.IntegerField(null=True, blank=True, default=0)
     total_errors = models.IntegerField(null=True, blank=True, default=0)
     scan_time = models.IntegerField(null=True, blank=True, default=0)
-    scan_info = models.CharField(max_length=3000)
 
 
+# Single result for each file being scanned
+class ScanFileInfo(models.Model):
+    def __str__(self):
+        return self.file_path
+
+    scan_result = models.ForeignKey(ScanResult)
+    file_path = models.CharField(max_length=400)
+
+# Multiple or no result for each file
 class License(models.Model):
     def __str__(self):
         return self.license
-
-    result_info = models.ForeignKey(ScannedResults)
+    
+    scan_file_info = models.ForeignKey(ScanFileInfo)
     license = models.CharField(max_length=1000)
 
 
 class Copyright(models.Model):
     def __str__(self):
         return self.copyright
-
-    result_info = models.ForeignKey(ScannedResults)
+    
+    scan_file_info = models.ForeignKey(ScanFileInfo)
     copyright = models.CharField(max_length=1000)
 
 
 class Package(models.Model):
     def __str__(self):
         return self.package
-
-    result_info = models.ForeignKey(ScannedResults)
+    
+    scan_file_info = models.ForeignKey(ScanFileInfo)
     package = models.CharField(max_length=1000)
+
+
+class ScanError(models.Model):
+    def __str__(self):
+        return self.scan_error
+    
+    scan_file_info = models.ForeignKey(ScanFileInfo)
+    scan_error = models.CharField(max_length=1000)
