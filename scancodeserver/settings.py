fdf8da0ab2b578ddcff323bc25d41c5220ade4fe
rajukoushik <g.rajukoushik@gmail.com>
2017-06-17 02:23:37 +0530

    Updated .travis.yml
    *Integrated the repository with travis ci

--- scancodeserver/settings.py
+++ scancodeserver/settings.py
@@ -12,6 +12,23 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+# travis ci integration
+# (...)
+
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'travisci',
+            'USER': 'postgres',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
+
+    # (...)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -82,7 +99,6 @@ DATABASES = {
         'PASSWORD': 'USER_PASSWORD',
         'HOST': 'localhost',
         'PORT': '',
-
 
     }
 }
