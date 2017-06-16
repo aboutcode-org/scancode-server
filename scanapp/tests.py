ba07d11a9f8c7183cd8977f745be4b963fdfe214
RajuKoushik <g.rajukoushik@gmail.com>
2017-06-16 23:38:57 +0530

    Updated tests.py.
    rr

--- scanapp/tests.py
+++ scanapp/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
-
 # Create your tests here.
+import os
+import sys
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'test_settings'
+test_dir = os.path.dirname(__file__)
+sys.path.insert(0, test_dir)
+
+from django.test.utils import get_runner
+from django.conf import settings
+
+
+def runtests():
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner(verbosity=1, interactive=True)
+    failures = test_runner.run_tests(['your_app'])
+    sys.exit(bool(failures))
