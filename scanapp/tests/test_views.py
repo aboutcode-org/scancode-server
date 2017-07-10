cbe5b40f3eb7dca7603ed02431631b473e38022e
singh1114 <ranvir.singh1114@gmail.com>
2017-07-11 02:41:27 +0530

    Adding tests for ScanResult and LoginView
    Signed-off-by: Ranvir Singh ranvir.singh1114@gmail.com

--- scanapp/tests/test_views.py
+++ scanapp/tests/test_views.py
@@ -4,6 +4,8 @@ from django.test import RequestFactory
 
 from scanapp.views import LocalUploadView
 from scanapp.views import URLFormView
+from scanapp.views import ScanResults
+from scanapp.views import LoginView
 
 from django.contrib.auth.models import User
 from django.contrib.auth.models import AnonymousUser
@@ -86,3 +88,76 @@ class LocalUploadViewTestCase(TestCase):
         request.user = AnonymousUser()
         response = LocalUploadView.as_view()(request)
         self.assertIsNone(response)
+
+class ScanResultViewTestCase(TestCase):
+    def test_scan_result_view_anonymous_user_get_request(self):
+        request = RequestFactory().post('/urlscan/', {'URL': 'https://github.com/'})
+        request.user = AnonymousUser()
+        response = URLFormView.as_view()(request)
+        url = response.url
+        digits_in_url = url[12:]
+        scan_id = ''
+        for character in digits_in_url:
+            try:
+                scan_id = scan_id + str(int(character))
+            except:
+                pass
+        scan_id = int(scan_id)
+        request = RequestFactory().get('/scanresult/')
+        response = ScanResults.as_view()(request, pk=scan_id)
+        self.assertEqual('<h3>Please wait... Your tasks are in the queue.\n Reload in 5-10 minutes</h3>\n', response.content)
+
+    def test_scan_result_view_anonymous_user_post_request(self):
+        request = RequestFactory().post('/urlscan/', {'URL': 'https://github.com/'})
+        request.user = AnonymousUser()
+        response = URLFormView.as_view()(request)
+        url = response.url
+        digits_in_url = url[12:]
+        scan_id = ''
+        for character in digits_in_url:
+            try:
+                scan_id = scan_id + str(int(character))
+            except:
+                pass
+        scan_id = int(scan_id)
+        request = RequestFactory().post('/scanresult/', {'post_apple': 'Apple'})
+        response = ScanResults.as_view()(request, pk=scan_id)
+        self.assertEqual(405, response.status_code)
+
+    def test_scan_result_view_registered_user_get_request(self):
+        user = User.objects.create_user(username='Username', password='Password')
+        request = RequestFactory().post('/urlscan/', {'URL': 'https://github.com/'})
+        request.user = user
+        response = URLFormView.as_view()(request)
+        url = response.url
+        digits_in_url = url[12:]
+        scan_id = ''
+        for character in digits_in_url:
+            try:
+                scan_id = scan_id + str(int(character))
+            except:
+                pass
+        scan_id = int(scan_id)
+        request = RequestFactory().get('/scanresult/')
+        response = ScanResults.as_view()(request, pk=scan_id)
+        self.assertEqual('<h3>Please wait... Your tasks are in the queue.\n Reload in 5-10 minutes</h3>\n', response.content)
+
+class LoginViewTestCase(TestCase):
+    def test_login_view_anonymous_user_get_request(self):
+        request = RequestFactory().get('/login')
+        request.user = AnonymousUser()
+        response = LoginView.as_view()(request)
+        self.assertEqual(200, response.status_code)
+
+    def test_login_view_registered_user_get_request(self):
+        user = User.objects.create_user(username='Username', password='Password')
+        request = RequestFactory().get('/login')
+        request.user = user
+        response = LoginView.as_view()(request)
+        self.assertEqual(302, response.status_code)
+
+    def test_login_view_anonymous_user_post_request(self):
+        request = RequestFactory().post('/login', {'username': 'username', 'password': 'password'})
+        request.user = AnonymousUser()
+        response = LoginView.as_view()(request)
+        self.assertEqual(str(response.context), 'username') 
