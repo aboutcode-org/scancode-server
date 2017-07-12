c01809d833d4606633d42ba3e722c63b50b29142
singh1114 <ranvir.singh1114@gmail.com>
2017-07-13 02:59:49 +0530

    Add serializers.py to serialize the models
    Signed-off-by: Ranvir Singh ranvir.singh1114@gmail.com

---
+++ scanapp/serializers.py
@@ -0,0 +1,169 @@
+from rest_framework import serializer
+
+from scanapp.models import ScanInfo
+from scanapp.models import UserInfo
+from scanapp.models import URLScanInfo
+from scanapp.models import LocalScanInfo
+from scanapp.models import CodeInfo
+from scanapp.models import ScanResult
+from scanapp.models import ScanFileInfo
+from scanapp.models import License
+from scanapp.models import MatchedRule
+from scanapp.models import MatchedRuleLicenses
+from scanapp.models import Copyright
+from scanapp.models import CopyrightHolders
+from scanapp.models import CopyrightStatements
+from scanapp.models import CopyrightAuthor
+from scanapp.models import Package
+from scanapp.models import ScanError
+
+class ScanInfoSerializer(serializers.ModelSerializer):
+    """
+    ModelSerializer for `ScanInfo` model with all fields
+    """
+    class Meta:
+        model = ScanInfo
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    ModelSerializer for `User` model
+    """
+    class Meta:
+        model = User
+
+class UserInfoSerializer(serializers.ModelSerializer):
+    """
+    ModelSerializer for `UserInfo` model with all field
+    """
+    user = UserSerializer()
+    scan_info_serializer = ScanInfoSerializer()
+    class Meta:
+        model = UserInfo
+        fields = '__all__'
+
+class UrlScanInfoSerializer(serializers.ModelSerializer):
+    """
+    ModelSerializer for `URLScanInfo` with all fields
+    """
+    scan_info_serializer = ScanInfoSerializer()
+    class Meta:
+        model = URLScanInfo
+        fields = '__all__'
+
+class LocalScanInfoSerializer(serializers.ModelSerializer):
+    """
+    ModelSerializer for `LocalScanInfo` with all fields
+    """
+    scan_info_serializer = ScanInfoSerializer()
+    class Meta:
+        model = LocalScanInfo
+        fields = '__all__'
+
+class CodeInfoSerializer(serializers.ModelSerializer):
+    """
+    ModelSerializer for `CodeInfo` with all fields
+    """
+    scan_info_serializer = ScanInfoSerializer()
+    class Meta:
+        model = CodeInfo
+        fields = '__all__'
+
+class ScanResultSerializer(serializers.ModelSerializer):
+    """
+    ModelSerializer for `ScanResult` with all fields
+    """
+    code_info_serializer = CodeInfoSerializer()
+    class Meta:
+        model = ScanResult
+        fields = '__all__'
+
+class ScanFileInfoSerializer(serializers.ModelSerializer):
+    """
+    ModelSerializer for `ScanFileInfo` with all fields
+    """
+    scan_result_serializer = ScanResultSerializer()
+    class Meta:
+        model = ScanFileInfo
+        fields = '__all__'
+
+class LicenseSerializer(serializers.ModelSerializer):
+    """
+    ModelSerializer for `License` with all fields
+    """
+    scan_file_info_serializer = ScanFileInfoSerializer()
+    class Meta:
+        model = License
+        fields = '__all__'
+
+class MatchedRuleSerializer(serializers.ModelSerializer):
+    """
+    ModelSerializer for `MatchedRule` with all fields
+    """
+    license_serializer = LicenseSerializer()
+    class Meta:
+        model = MatchedRule
+        fields = '__all__'
+
+class MatchedRuleLicenseSerializer(serializers.ModelSerializer):
+    """
+    ModelSerializer for `MatchedRuleLicense` with all fields
+    """
+    license_serializer = MatchedRuleSerializer()
+    class Meta:
+        model = MatchedRuleLicense
+        fields = '__all__'
+
+class CopyrightSerializer(serializers.ModelSerializer):
+    """
+    ModelSerializer for `Copyright` with all fields
+    """
+    scan_file_info_serializer = ScanFileInfoSerializer()
+    class Meta:
+        model = Copyright
+        fields = '__all__'
+
+class CopyrightHolderSerializer(serializers.ModelSerializer):
+    """
+    ModelSerializer for `CopyrightHolders` with all fields
+    """
+    copyright_serializer = CopyrightSerializer()
+    class Meta:
+        model = CopyrightHolders
+        fields = '__all__'
+
+class CopyrightStatementSerializer(serializers.ModelSerializer):
+    """
+    ModelSerializer for `CopyrightHolders` with all fields
+    """
+    copyright_serializer = CopyrightSerializer()
+    class Meta:
+        model = CopyrightStatements
+        fields = '__all__'
+
+class CopyrightAuthorSerializer(serializers.ModelSerializer):
+    """
+    ModelSerializer for `CopyrightAuthor` with all fields
+    """
+    copyright_serializer = CopyrightSerializer()
+    class Meta:
+        model = CopyrightAuthor
+        fields = '__all__'
+
+class PackageSerializer(serializers.ModelSerializer):
+    """
+    ModelSerializer for `Package` with all fields
+    """
+    scan_file_info_serializer = ScanFileInfoSerializer()
+    class Meta:
+        model = Package
+        fields = '__all__'
+
+class PackageSerializer(serializers.ModelSerializer):
+    """
+    ModelSerializer for `Package` with all fields
+    """
+    scan_file_info_serializer = ScanFileInfoSerializer()
+    class Meta:
+        model = ScanError
+        fields = '__all__'
