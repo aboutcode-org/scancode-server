861abd7b7960fe0d387f7649e756980e08685c8d
Philippe Ombredanne <pombredanne@nexb.com>
2017-07-26 16:53:50 +0200

    Use default for PosgreSQL DB, user and password
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- scancodeserver/settings.py
+++ scancodeserver/settings.py
@@ -35,22 +35,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
-# travis ci integration
-# (...)
-
-if 'TRAVIS' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'travisci',
-            'USER': 'postgres',
-            'PASSWORD': '',
-            'HOST': 'localhost',
-            'PORT': '',
-        }
-    }
-
-    # (...)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -113,18 +97,23 @@ WSGI_APPLICATION = 'scancodeserver.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+# travis ci integration
+# (...)
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'DATABASE_NAME',
-        'USER': 'DATABASE_USER',
-        'PASSWORD': 'USER_PASSWORD',
+        'NAME': 'scancode',
+        'USER': 'scancode',
+        'PASSWORD': 'scancode',
         'HOST': 'localhost',
-        'PORT': '',
+        'PORT': '5432',
 
     }
 }
+if 'TRAVIS' in os.environ:
+    DATABASES['default']['USER'] = 'postgres'
+    DATABASES['default']['PASSWORD'] = ''
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
@@ -183,12 +172,12 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# CELERY STUFF
+# CELERY Configuration
 BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'Africa/Nairobi'
+CELERY_TIMEZONE = 'UTC'
 
 LOGIN_REDIRECT_URL = '/localscan'
