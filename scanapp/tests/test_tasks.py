2b3d25ef6d4a86ea69507634b4e5e2a8669ebfd4
singh1114 <ranvir.singh1114@gmail.com>
2017-07-31 17:33:10 +0530

    Remove commented code
    * sort the imports
    
    Signed-off-by: Ranvir Singh ranvir.singh1114@gmail.com

--- scanapp/tests/test_tasks.py
+++ scanapp/tests/test_tasks.py
@@ -23,59 +23,20 @@
 
 import json
 
-from django.test import TestCase
-from django.core.files import File
-from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.files import File
+from django.test import TestCase
+from django.utils import timezone
 
-from scanapp.tasks import scan_code_async
 from scanapp.tasks import apply_scan_async
-from scanapp.tasks import save_results_to_db
 from scanapp.tasks import create_scan_id
 from scanapp.tasks import fill_unfilled_scan_model
+from scanapp.tasks import save_results_to_db
+from scanapp.tasks import scan_code_async
 
 from scanapp.models import Scan
 from scanapp.models import ScannedFile
 from scanapp.models import License
-
-
-#class SaveResultsToDBTestCase(TestCase):
-#    def test_correct_save_result_to_db_with_scan_type_url(self):
-#        # TODO function not complete add more stuff to scanapp/tests/sample_json.txt
-#        # to test more
-#        url = 'https://github.com/'
-#        scan_id = create_scan_id()
-#        folder_name = None
-#        scan_type = 'URL'
-#        json_file = File(open('scanapp/tests/sample_json.txt', 'r'))
-#        for text in json_file:
-#            json_data = text
-#
-#        json_data = json.loads(json_data)
-#        save_results_to_db(scan_id, json_data, scan_type, URL, folder_name)
-#        # assert statements tests each database
-#        scan_info = Scan.objects.get(pk=scan_id)
-#
-#        code_info = CodeInfo.objects.get(scan_info=scan_info)
-#        self.assertEqual(json_data['files_count'], code_info.total_code_files)
-#
-#        url_scan_info = URLScan.objects.get(scan_info=scan_info)
-#        self.assertEqual('https://github.com/', url_scan_info.URL)
-#
-#        scan_result = ScanResult.objects.get(code_info=code_info)
-#        self.assertEqual(json_data, scan_result.scanned_json_result)
-#
-#        scan_file_info = ScanFileInfo.objects.get(scan_result=scan_result)
-#        self.assertEqual(json_data['files'][0]['path'], scan_file_info.file_path)
-#
-#        license = License.objects.get(scan_file_info=scan_file_info)
-#        self.assertEqual(json_data['files'][0]['licenses'][0]['key'], license.key)
-#
-#        matched_rule = MatchedRule.objects.get(license=license)
-#        self.assertEqual(json_data['files'][0]['licenses'][0]['matched_rule']['identifier'], matched_rule.identifier)
-#
-#        matched_rule_license = MatchedRuleLicenses.objects.get(license=license)
-#        self.assertEqual(json_data['files'][0]['licenses'][0]['matched_rule']['licenses'][0], matched_rule_license.license)
 
 
 class CreateScanIdTestCase(TestCase):
